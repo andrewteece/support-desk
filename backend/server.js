55148d3ae9dafc8dc83abc6fe560bd087159e319
andrewteece <andrewteece@gmail.com>
2024-09-21 17:30:58 -0500

    userRoutes and userController code

--- backend/server.js
+++ backend/server.js
@@ -7,4 +7,7 @@ app.get('/', (req, res) => {
     res.send('Hello')
 })
 
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
