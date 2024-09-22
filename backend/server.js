1a9ec11ba428cb2cb8a3b3b09dacb4e8b73b762b
andrewteece <andrewteece@gmail.com>
2024-09-22 08:44:23 -0500

    install express-async-handler and setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middelware/errorMiddleware')
 const PORT = process.env.PORT || 8000
 const app = express()
 
@@ -12,5 +13,7 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
