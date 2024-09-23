43ccfab27fb5e852215f0373e6f1ab5805529483
andrewteece <andrewteece@gmail.com>
2024-09-23 17:05:19 -0500

    add token to env and apply to userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -39,7 +39,8 @@ if(user) {
     res.status(201).json({
         _id: user._id,
         name: user.name,
-        email: user.email
+        email: user.email,
+        token: generateToken(user._id)
     })
 } else {
     res.status(400)
@@ -61,13 +62,21 @@ const loginUser = asyncHandler(async (req, res) => {
     res.json(200).json({
         _id:  user._id,
         name: user.name,
-        email: user.email
+        email: user.email,
+        token: generateToken(user._id)
     })
    } else {
     res.status(401)
     throw new Error('Invalid credentials')
    }
 })
+
+// generate token
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+}
 
 module.exports = {
     registerUser,
