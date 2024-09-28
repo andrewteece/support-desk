e1499cafa8d8f7710f50c26501d6d7c63343dc67
andrewteece <andrewteece@gmail.com>
2024-09-28 17:54:47 -0500

    fixed typo for users/

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,35 +1,34 @@
-import axios from "axios";
+import axios from 'axios'
 
+const API_URL = '/api/users/'
 
-const API_URL = '/api/users'
-
-// register user
+// Register user
 const register = async (userData) => {
-    const response = await axios.post(API_URL, userData)
+  const response = await axios.post(API_URL, userData)
 
-    if (response.data) {
-        localStorage.setItem('user', JSON.stringify(response.data))
-    }
-    return response.data
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data))
+  }
+  return response.data
 }
 
-// login user
+// Login user
 const login = async (userData) => {
-    const response = await axios.post(API_URL + 'login', userData)
+  const response = await axios.post(API_URL + 'login', userData)
 
-    if (response.data) {
-        localStorage.setItem('user', JSON.stringify(response.data))
-    }
-    return response.data
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data))
+  }
+  return response.data
 }
 
-// logout user
+// Logout user
 const logout = () => localStorage.removeItem('user')
 
 const authService = {
-    register,
-    logout,
-    login,
+  register,
+  logout,
+  login,
 }
 
 export default authService
