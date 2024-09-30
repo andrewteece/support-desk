65454745f94ed2b2162a568a9f1abd980c517708
andrewteece <andrewteece@gmail.com>
2024-09-30 09:36:04 -0500

    BackButton component code

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom'
-import { toast } from 'react-toastify'
-import { createTicket } from '../features/tickets/ticketSlice'
-import 
-
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { createTicket } from '../features/tickets/ticketSlice';
+import BackButton from '../components/BackButton';
 function NewTicket() {
   const { user } = useSelector((state) => state.auth);
 
