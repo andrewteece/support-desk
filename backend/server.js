fb20dafd6cd7c041145e0172bbd041f3d3da198f
andrewteece <andrewteece@gmail.com>
2024-10-01 14:31:40 -0500

    fixed vite.config proxy port

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,21 @@ app.get('/', (req, res) => {
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+    // Set build folder as static
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+  
+    // FIX: below code fixes app crashing on refresh in deployment
+    app.get('*', (_, res) => {
+      res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+    })
+  } else {
+    app.get('/', (_, res) => {
+      res.status(200).json({ message: 'Welcome to the Support Desk API' })
+    })
+  }
+
 app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    // port: 6000,
     proxy: {
-      "/api": "http://localhost:5000",
+      "/api": "http://localhost:6000",
     }
   }
 })
