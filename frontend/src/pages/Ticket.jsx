0a9d34b544ec26f199dc035e6eb7bdeb770c238b
andrewteece <andrewteece@gmail.com>
2024-10-02 08:14:00 -0500

    Ticket code and add to App

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -8,4 +8,142 @@ import { getNotes, createNote } from '../features/notes/noteSlice';
 import { useParams, useNavigate } from 'react-router-dom';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
-import NoteItem from '../components/NoteIem';
+import NoteItem from '../components/NoteItem';
+
+const customStyles = {
+  content: {
+    width: '600px',
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    position: 'relative',
+  },
+};
+
+Modal.setAppElement('#root');
+
+function Ticket() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [noteText, setNoteText] = useState('');
+  const { ticket } = useSelector((state) => state.tickets);
+
+  const { notes } = useSelector((state) => state.notes);
+
+  // NOTE: no need for two useParams
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { ticketId } = useParams();
+
+  useEffect(() => {
+    dispatch(getTicket(ticketId)).unwrap().catch(toast.error);
+    dispatch(getNotes(ticketId)).unwrap().catch(toast.error);
+  }, [ticketId, dispatch]);
+
+  // close ticket
+  const onTicketClose = () => {
+    dispatch(closeTicket(ticketId))
+      .unwrap()
+      .then(() => {
+        toast.success('Ticket closed');
+        navigate('/tickets');
+      })
+      .catch(toast.error);
+  };
+
+  // create note submit
+  const onNoteSubmit = (e) => {
+    e.preventDeafault();
+    dispatch(createNote({ noteText, ticketId }))
+      .unwrap()
+      .then(() => {
+        setNoteText('');
+        closeModal();
+      })
+      .catch(toast.error);
+  };
+
+  // open/close modal
+  const openModal = () => setModalIsOpen(true);
+  const closeModal = () => setModalIsOpen(false);
+
+  if (!ticket) {
+    return <Spinner />;
+  }
+
+  return (
+    <div className='ticket-page'>
+      <header className='ticket-header'>
+        <BackButton />
+        <h2>
+          Ticket ID: {ticket._id}
+          <span className={`status status-${ticket.staus}`}>
+            {ticket.status}
+          </span>
+        </h2>
+        <h3>
+          Date Submitted:{' '}
+          {new Date(ticket.createdAt).toLocaleDateString('en-US')}
+        </h3>
+        <h3>Product: {ticket.product}</h3>
+        <hr />
+        <div className='ticket-desc'>
+          <h3>Desription of Issue</h3>
+          <p>{ticket.description}</p>
+        </div>
+        <h2>Notes</h2>
+      </header>
+
+      {ticket.status !== 'closed' && (
+        <button onClick={openModal} className='btn'>
+          <FaPlus /> Add Note
+        </button>
+      )}
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClost={closeModal}
+        style={customStyles}
+        contentLabel='Add Note'
+      >
+        <h2>Add Note</h2>
+        <button className='btn-close' onClick={closeModal}>
+          X
+        </button>
+        <form onSubmit={onNoteSubmit}>
+          <div>
+            <textarea
+              name='noteText'
+              id='noteText'
+              className='form-control'
+              placeholder='Note text'
+              value={noteText}
+              onChange={(e) => setNoteText(e.target.value)}
+            ></textarea>
+          </div>
+          <div className='form-group'>
+            <button className='btn' type='submit'>
+              Submit
+            </button>
+          </div>
+        </form>
+      </Modal>
+
+      {notes ? (
+        notes.map((note) => <NoteItem key={note._id} note={note} />)
+      ) : (
+        <Spinner />
+      )}
+
+      {ticket.status !== 'closed' && (
+        <button onClick={onTicketClose} className='btn btn-block btn-danger'>
+          Close Ticket
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default Ticket;
