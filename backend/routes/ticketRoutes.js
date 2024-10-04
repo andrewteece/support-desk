52e605858fe2cf8308ee0fa669b54d078c82f054
andrewteece <andrewteece@gmail.com>
2024-10-04 10:09:53 -0500

    refactor ticketController, ticketRoutes, ticketSlice

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,26 +1,27 @@
 const express = require('express')
 const router = express.Router()
-const {
-    getTickets,
-    getTicket,
-    createTicket,
-    deleteTicket,
-    updateTicket,
-} = require('../controllers/ticketController')
+const { getTickets, createTicket } = require('../controllers/ticketController') 
+// const {
+//     getTickets,
+//     getTicket,
+//     createTicket,
+//     deleteTicket,
+//     updateTicket,
+// } = require('../controllers/ticketController')
 
 const { protect } = require('../middelware/authMiddleware')
 
 
 // Re-route into note router
-const noteRouter = require('./noteRoutes')
-router.use('/:ticketId/notes', noteRouter)
+// const noteRouter = require('./noteRoutes')
+// router.use('/:ticketId/notes', noteRouter)
 
 router.route('/').get(protect, getTickets).post(protect, createTicket)
 
-router
-    .route('/')
-    .get(protect, getTicket)
-    .delete(protect, deleteTicket)
-    .put(protect, updateTicket)
+// router
+//     .route('/')
+//     .get(protect, getTicket)
+    // .delete(protect, deleteTicket)
+    // .put(protect, updateTicket)
 
 module.exports = router
--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -38,17 +38,36 @@ export const getTickets = createAsyncThunk(
 )
 
 // Get user ticket
+// export const getTicket = createAsyncThunk(
+//   'tickets/get',
+//   async (ticketId, thunkAPI) => {
+//     try {
+//       const token = thunkAPI.getState().auth.user.token
+//       return await ticketService.getTicket(ticketId, token)
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue(extractErrorMessage(error))
+//     }
+//   }
+// )
+
 export const getTicket = createAsyncThunk(
-  'tickets/get',
-  async (ticketId, thunkAPI) => {
-    try {
-      const token = thunkAPI.getState().auth.user.token
-      return await ticketService.getTicket(ticketId, token)
-    } catch (error) {
-      return thunkAPI.rejectWithValue(extractErrorMessage(error))
+    'tickets/get',
+    async (ticketId, thunkAPI) => {
+      try {
+        const token = thunkAPI.getState().auth.user.token
+        return await ticketService.getTicket(ticketId, token)
+      } catch (error) {
+        const message = (
+          error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString()
+        return thunkAPI.rejectWithValue(message)
+      }
     }
-  }
-)
+  )
+
 
 // Close ticket
 export const closeTicket = createAsyncThunk(
