6b05d13cd440dcca3b7e1ba3e221ac8093b75164
andrewteece <andrewteece@gmail.com>
2024-10-05 08:00:53 -0500

    refactor PrivateRoute

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,11 +1,26 @@
-import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+// import { Navigate } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
 
-const PrivateRoute = ({ children }) => {
-  const { user } = useSelector((state) => state.auth);
+// const PrivateRoute = ({ children }) => {
+//   const { user } = useSelector((state) => state.auth);
 
-  if (user) return children;
-  return <Navigate to='/login' />;
-};
+//   if (user) return children;
+//   return <Navigate to='/login' />;
+// };
 
-export default PrivateRoute;
+// export default PrivateRoute;
+
+import { Navigate, Outlet } from 'react-router-dom'
+import { useAuthStatus } from '../hooks/useAuthStatus'
+import Spinner from './Spinner'
+
+const PrivateRoute = () => {
+  const { loggedIn, checkingStatus } = useAuthStatus()
+
+  if (checkingStatus) {
+    return <Spinner />
+  }
+  return loggedIn ? <Outlet /> : <Navigate to={'/login'} />
+}
+
+export default PrivateRoute
